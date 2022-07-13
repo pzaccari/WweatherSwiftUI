5b0b5a0030f13fddd4f561b19c6a92fb7f00f8b3
pzaccari <pzaccari@stanthony.com>
2022-07-13 13:47:24 -0500

    Initial Commit

---
+++ WweatherSwiftUI/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WweatherSwiftUI
+//
+//  Created by Phil Zaccari on 7/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {Color.blue.edgesIgnoringSafeArea(.all)}
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WweatherSwiftUI/WweatherSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WweatherSwiftUIApp.swift
+//  WweatherSwiftUI
+//
+//  Created by Phil Zaccari on 7/13/22.
+//
+
+import SwiftUI
+
+@main
+struct WweatherSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
